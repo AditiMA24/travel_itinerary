f237aed5e89ebc4cbc49d1404badb76e0e5ea99e
GbyAtee <gby.atee_ma24@ashoka.edu.in>
2023-04-08 12:24:43 +0530

    gby_day3

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,12 @@ from datetime import datetime
 
 
 
-
+@app.route('/test_output')
+def test_output():
+    # user = {'name': 'Gby'}
+    return render_template('test_output.html', title='test_Output') #,user=user            
+    
+        
 
 @app.route('/questionnaire', methods=['GET', 'POST'])
 def questionnaire():
@@ -30,25 +35,39 @@ def questionnaire():
     # destination = request.form['destination']
     # # Render the results template with the generated itinerary
     if form.validate_on_submit():
-        return redirect(url_for('submit_form'))
-        # flash('Login requested for user {}, remember_me={}'.format(
-        #     form.username.data, form.remember_me.data))
+        # session['Destination'] = form.destination.data
+        # session['Start Date'] = form.start.data
+        # session['End Date'] = form.end.data
+        # session['duration'] = datetime.strptime(session['End Date'], "%Y/%m/%d") - datetime.strptime(session['Start Date'], "%Y/%m/%d")
+        flash('Login requested for'.format(
+            form.destination.data))
+        
+        # return redirect(url_for('test_output'))
+        # return redirect(url_for('output'))
+        # flash('Login requested for {}'.format(
+            # form.name.data))   
         # return redirect(url_for('index'))
 
     return render_template('questionnaire.html',  title='TellUs', form=form)
+
+
 
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     form = LoginForm()
     if form.validate_on_submit():
+
         session['Destination'] = form.destination.data
         session['Start Date'] = form.start.data
         session['End Date'] = form.end.data
         session['duration'] = datetime.strptime(session['End Date'], "%Y/%m/%d") - datetime.strptime(session['Start Date'], "%Y/%m/%d")
         # process the form data and present the output
-        return render_template('output.html', destination=session['Destination'], start_date=session['Start Date'], end_date=session['End Date'])
-    return render_template('form.html', form=form)
+        flash('Login requested for {}'.format(
+            session['Destination']))
+
+        # return render_template('output.html', destination=session['Destination'], start_date=session['Start Date'], end_date=session['End Date'])
+    # return render_template('form.html', form=form)
 
 # if __name__ == '__main__':
 #     app.run()
@@ -86,7 +105,7 @@ def form():
 #     #     result += choice.message.content
 
 #     # # Render the results template with the generated itinerary
-    return render_template('submit_form.html',  title='Processing')
+    # return render_template('submit_form.html',  title='Processing')
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
