9f1f251df63757684c872d8dd6b2c1ecf6ceeb2b
GbyAtee <gby.atee_ma24@ashoka.edu.in>
2023-05-16 00:04:02 +0530

    graph and result worked! GEM version 1

--- app/routes.py
+++ app/routes.py
@@ -25,7 +25,7 @@ from IPython.display import display
 import re
 
 ##KEYS
-openai.api_key = "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"
+openai.api_key = ""
 openai.Model.list();
 dist_key="ScsWl6XfkOpZd2KHCATUMQpsHcNtX"
 
@@ -205,7 +205,7 @@ def questionnaire():
                             model="gpt-3.5-turbo",
                             messages=[
                                     {"role": "system", "content": "You are a chatbot"},
-                                    {"role": "user", "content": f"Generate an itineary for a {duration} trip to {place}."},
+                                    {"role": "user", "content": f"Generate an itineary for a {duration} day trip to {place}."},
                                     {"role": "user", "content": f"I am only interested in {preference}. "},
                                     {"role": "user", "content": "Format the result in such a way that, 1. Capitalize AM and PM. 2. Separate the time and description by a colon."},
                                     {"role": "user", "content": "3. Print the addresses at the end of each suggestion but strictly on the same line. The address is one that I must search on Google maps "},
@@ -475,75 +475,73 @@ def questionnaire():
                 itinerary_destination_set=list(itinerary['destination'])
                 suggestion_destination_set=list(suggestion['destination'])
             #     return f"{df['destination'][0]}"
-            # except Exception as e:
-            #     print(f"An error occurred: {str(e)}")
-            #     return "There was an error in processing. Please try again."
+            
+                ####CREATE map html
+                # Create a map centered on India
+                india_map = folium.Map(location=[20.5937, 78.9629], zoom_start=5)
 
-                # # ####CREATE map html
-                # # # Create a map centered on India
-                # # india_map = folium.Map(location=[20.5937, 78.9629], zoom_start=5)
+                # Create a dictionary to store colors for each day
+                color_map = {
+                    1: 'blue',
+                    2: 'green',
+                    3: 'red',
+                    4: 'purple',
+                    5: 'yellow',
+                    6: 'pink',
+                    7: 'orange',
+                    8: 'cyan',
+                    9: 'magenta',
+                    10: 'brown',
+                    11: 'gray',
+                    12: 'olive',
+                    13: 'lime',
+                    14: 'teal',
+                    15: 'navy'
+                }
 
-                # # # Create a dictionary to store colors for each day
-                # # color_map = {
-                # #     1: 'blue',
-                # #     2: 'green',
-                # #     3: 'red',
-                # #     4: 'purple',
-                # #     5: 'yellow',
-                # #     6: 'pink',
-                # #     7: 'orange',
-                #     8: 'cyan',
-                #     9: 'magenta',
-                #     10: 'brown',
-                #     11: 'gray',
-                #     12: 'olive',
-                #     13: 'lime',
-                #     14: 'teal',
-                #     15: 'navy'
-                # }
+                def extract_coordinates(lat_lon_str):
+                    coordinates = re.findall(r'\d+\.\d+', lat_lon_str)
+                    return [float(coord) for coord in coordinates]
 
-                # def extract_coordinates(lat_lon_str):
-                #     coordinates = re.findall(r'\d+\.\d+', lat_lon_str)
-                #     return [float(coord) for coord in coordinates]
+                destination_data = df[df['within_time_limit'] == True].groupby('day', group_keys=True)[['destination', 'Latitude', 'Longitude']].apply(lambda x: [[row[0], *extract_coordinates(row[1]), *extract_coordinates(row[2])] for _, row in x.iterrows()]).to_dict()
 
-                # destination_data = df[df['within_time_limit'] == True].groupby('day')[['destination', 'Latitude', 'Longitude']].apply(lambda x: [[row[0], *extract_coordinates(row[1]), *extract_coordinates(row[2])] for _, row in x.iterrows()]).to_dict()
+                # Create a marker cluster group
+                marker_cluster = MarkerCluster().add_to(india_map)
 
-                # # Create a marker cluster group
-                # marker_cluster = MarkerCluster().add_to(india_map)
+                # Iterate over the destination_data dictionary
+                for day, destinations in destination_data.items():
+                    # Get the color for the current day
+                    color = color_map.get(day, 'gray')
 
-                # # Iterate over the destination_data dictionary
-                # for day, destinations in destination_data.items():
-                #     # Get the color for the current day
-                #     color = color_map.get(day, 'gray')
+                    # Create a feature group for markers and polyline of the current day
+                    day_group = folium.FeatureGroup(name=f"Day {day}")
 
-                #     # Create a feature group for markers and polyline of the current day
-                #     day_group = folium.FeatureGroup(name=f"Day {day}")
+                    # Iterate over destinations of the current day
+                    for i, (destination, lat, lon) in enumerate(destinations, start=1):
+                        # Add a marker with the corresponding color and number
+                        folium.Marker(
+                            location=[lat, lon],
+                            popup=destination,
+                            icon=folium.Icon(color=color),
+                            tooltip=str(i)  # Set the tooltip as the marker number
+                        ).add_to(marker_cluster)  # Add the marker to the marker cluster
 
-                #     # Iterate over destinations of the current day
-                #     for i, (destination, lat, lon) in enumerate(destinations, start=1):
-                #         # Add a marker with the corresponding color and number
-                #         folium.Marker(
-                #             location=[lat, lon],
-                #             popup=destination,
-                #             icon=folium.Icon(color=color),
-                #             tooltip=str(i)  # Set the tooltip as the marker number
-                #         ).add_to(marker_cluster)  # Add the marker to the marker cluster
+                    # Create a polyline for the current day
+                    locations = [(lat, lon) for _, lat, lon in destinations]
+                    folium.PolyLine(locations, color=color, weight=3, opacity=0.8, smooth_factor=1).add_to(day_group)
 
-                #     # Create a polyline for the current day
-                #     locations = [(lat, lon) for _, lat, lon in destinations]
-                #     folium.PolyLine(locations, color=color, weight=3, opacity=0.8, smooth_factor=1).add_to(day_group)
+                    # Add the feature group to the map
+                    day_group.add_to(india_map)
 
-                #     # Add the feature group to the map
-                #     day_group.add_to(india_map)
+                # Adjust the zoom to fit the markers and polylines
+                india_map.fit_bounds(india_map.get_bounds())
 
-                # # Adjust the zoom to fit the markers and polylines
-                # india_map.fit_bounds(india_map.get_bounds())
+                # Add a layer control to the map
+                folium.LayerControl().add_to(india_map)
 
-                # # Add a layer control to the map
-                # folium.LayerControl().add_to(india_map)
-
-                # # Save the map as an HTML file
-                # india_map.save('my_map.html')
+                # Save the map as an HTML file
+                india_map.save('my_map.html')
+                map_html = india_map._repr_html_()
 
                 # # Display the map in Colab
                 # display(india_map)
@@ -568,8 +566,8 @@ def questionnaire():
                 text = f"<p>{add}</p>"
 
                 # Concatenate the table HTML and the text
-                html_result = html_output + text 
-                return html_result, result #, india_map
+                html_result = html_output + text + map_html
+                return  html_result  
             
             except :
             # If there was an error in processing
